781281cf72e62297b871ce531ea91a1db2f9a0be
terrence-huang <terrencehuang2000@gmail.com>
2017-11-27 16:43:03 +0800

    update accordion-recursive-page

--- src/app/components/accordion-recursive-page/accordion-recursive-page.component.ts
+++ src/app/components/accordion-recursive-page/accordion-recursive-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-accordion-recursive-page',
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AccordionRecursivePageComponent implements OnInit {
 
     @Input() first: boolean;
+    @Input() data: number;
+    @Output() dataChange = new EventEmitter<Object>();
 
     constructor() {
     }
